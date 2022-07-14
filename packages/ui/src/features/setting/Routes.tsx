6874c754c0338e42ec06ff94cf47c23990579fd7
xlboy <xlboy88888@foxmail.com>
2022-07-15 01:28:56 +0800

    wip(ui): setting-routes 文件大小写问题解决（100%）

---
+++ packages/ui/src/features/setting/Routes.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+import ViewShortcutKey from './view/ShortcutKey';
+
+function SettingRoutes(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="shortcut-key" element={<ViewShortcutKey />} />
+    </Routes>
+  );
+}
+
+export default SettingRoutes;
