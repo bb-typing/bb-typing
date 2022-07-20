ccc18eb28dcfe7b237cafa87221085c2e1197735
xlboy <xlboy88888@foxmail.com>
2022-07-21 00:33:47 +0800

    wip(ui): `react-hotkeys` 的 demo 完善

--- packages/ui/src/components/Welcome.tsx
+++ packages/ui/src/components/Welcome.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import type { KeyMap } from 'react-hotkeys';
+import { HotKeys } from 'react-hotkeys';
 
 interface WelcomeProps {}
 
-const Welcome: React.FC<WelcomeProps> = props => {
-  return <div>欢迎来到 bb-typewrite</div>;
+type a = UIEvent;
+
+const Welcome: React.FC<WelcomeProps> = () => {
+  return (
+    <div>
+      <HotKeys
+        keyMap={{
+          MOVE_UP: 'f3'
+        }}
+        handlers={{
+          MOVE_UP: event => {
+            event!.preventDefault();
+
+            console.log('可是，错的是这个世界啊？');
+          }
+        }}
+      >
+        <input />t<div>三呢？</div>
+        <div>
+          <HotKeys
+            keyMap={{
+              MOVE_UP_1: 'c'
+            }}
+            handlers={{
+              MOVE_UP_1: event => console.log('我是f3-ccc')
+            }}
+          >
+            <input />
+            哦哦哦2
+          </HotKeys>
+        </div>
+      </HotKeys>
+
+      <HotKeys
+        keyMap={{
+          MOVE_UP: 'c'
+        }}
+        handlers={{
+          MOVE_UP: event => console.log('我是cccccccccc')
+        }}
+      >
+        <input />
+        哦哦哦2
+      </HotKeys>
+    </div>
+  );
 };
 
 export default Welcome;
