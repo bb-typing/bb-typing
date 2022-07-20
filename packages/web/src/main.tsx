ac0adec64badcfc7e4ad3f25cf9f8a209abbe9a2
xlboy <xlboy88888@foxmail.com>
2022-07-21 00:25:06 +0800

    fix: react 18 版本降至 17，解决大部分库的不兼容

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,9 +1,10 @@
 import { App } from '@ui/App';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
