0dd152ae8ba66ad4ecc4c62c22eb48bb11121842
xlboy <xlboy88888@foxmail.com>
2022-09-19 12:03:35 +0800

    feat(ui): 新增 system-store

--- packages/ui/src/stores/system.ts
+++ packages/ui/src/stores/system.ts
@@ -1,14 +1,32 @@
-import produce from 'immer';
+import _ from 'lodash';
 import { createTrackedSelector } from 'react-tracked';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
-export interface SystemStoreState {}
+export interface SystemStoreState {
+  version: string;
+}
 
 interface SystemStoreActions {}
 
-export const useSystemStore = create(
-  immer<SystemStoreState & SystemStoreActions>(set => ({}))
+type Store = SystemStoreState & SystemStoreActions;
+
+export const useSystemStore = create<
+  Store,
+  [['zustand/persist', Store], ['zustand/immer', Store]]
+>(
+  persist(
+    immer(set => ({
+      //#region  //*=========== state ===========
+      version: '0.0.0'
+      //#endregion  //*======== state ===========
+    })),
+    {
+      name: 'bb-store-system',
+      partialize: state => _.pick<any, keyof SystemStoreState>(state, ['version'])
+    }
+  )
 );
 
 export const useTrackedSystemStore = createTrackedSelector(useSystemStore);
