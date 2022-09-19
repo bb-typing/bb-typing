08dd3b0153245ac51e918ff7ee82c2677974a5a2
xlboy <xlboy88888@foxmail.com>
2022-09-19 11:32:19 +0800

    feat: 去除 react 严格模式

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,11 +1,6 @@
 import { App } from '@ui/App';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root')!);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
